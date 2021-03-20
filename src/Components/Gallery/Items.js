12acc8d1ef44c68ed2814e50f8f5ff5a4fa2c15e
tonhwk <andres_btk@yahoo.com>
2021-03-20 10:17:24 -0600

    updating...

--- src/Components/Gallery/Items.js
+++ src/Components/Gallery/Items.js
@@ -67,8 +67,53 @@ const Items = [
     format: "png",
     id: "twelve",
   },
+  {
+    name: "Memsnake",
+    format: "gif",
+    id: "thirteen",
+  },
+  {
+    name: "Pepe Washington crossing the delaware",
+    format: "jpeg",
+    id: "fourteen",
+  },
+  {
+    name: "Laser Eye Punks - 4156 punk",
+    format: "png",
+    id: "fiftteen",
+  },
+  {
+    name: "MORTAL 910",
+    format: "gif",
+    id: "sixteen",
+  },
+  {
+    name: "Post Debt",
+    format: "gif",
+    id: "seventeen",
+  },
+  {
+    name: "I fall, You fall, We fall",
+    format: "gif",
+    id: "eighteen",
+  },
+  {
+    name: "POLITICS IS BULLSHIT",
+    format: "gif",
+    id: "nineteen",
+  },
+  {
+    name: "Crypto Art rides The Bull",
+    format: "gif",
+    id: "twenty",
+  },
+  {
+    name: "An 80s Power Ballad About CryptoPunks 4156 and 6965 Song A Day",
+    format: "gif",
+    id: "twentyone",
+  },
 ];
 
-//
+//An 80s Power Ballad About CryptoPunks 4156 and 6965 Song A Day
 
 export default Items;
